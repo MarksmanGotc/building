948b7d2899ffd3e3974f9413b94eff24bd52a0a0
MarksmanGotc <151929315+MarksmanGotc@users.noreply.github.com>
2024-10-29 19:35:07 +0200

    Update json-parse.js

--- json-parse.js
+++ json-parse.js
@@ -125,6 +125,24 @@ function calculateCost() {
     var wrapperElement = document.querySelector('.wrapper');
     
     costSummaryElement.innerHTML = '';
+    if (!document.getElementById('closeResults')) {
+        let closeButton = document.createElement('button');
+        closeButton.id = 'closeResults';
+        
+        closeButton.innerHTML = `
+            <span>Click here to go back and modify your selections</span>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512">
+                <path d="M345 137c9.4-9.4 9.4-24.6 0-33.9s-24.6-9.4-33.9 0l-119 119L73 103c-9.4-9.4-24.6-9.4-33.9 0s-9.4 24.6 0 33.9l119 119L39 375c-9.4 9.4-9.4 24.6 0 33.9s24.6 9.4 33.9 0l119-119L311 409c9.4-9.4 24.6-9.4 33.9 0s9.4-24.6 0-33.9l-119-119L345 137z"></path>
+            </svg>`;
+
+        closeButton.addEventListener('click', function() {
+            document.querySelector('.wrapper').style.display = 'block';
+            costSummaryElement.style.display = 'none';
+            window.scrollTo({ top: 0, behavior: 'smooth' });
+        });
+
+        costSummaryElement.prepend(closeButton);
+    }
 
     var buildingBlocks = document.querySelectorAll('.buildingBlock');
     for (let i = 0; i < buildingBlocks.length; i++) {
